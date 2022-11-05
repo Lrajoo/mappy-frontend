09912fb0158d26c83db3f4d2c9b540971cd4bccf
Lingess Rajoo <Lrajoo@purdue.edu>
2022-11-05 14:50:41 -0700

    Using env variable for google maps api key

--- src/index.tsx
+++ src/index.tsx
@@ -6,13 +6,13 @@ import App from "./app/App";
 import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
+const key = process.env.REACT_APP_GOOGLE_MAPS_API_KEY || "";
 root.render(
-  <React.StrictMode>
-    <LoadScript googleMapsApiKey="">
-      <App />
-    </LoadScript>
-  </React.StrictMode>
+  <LoadScript googleMapsApiKey={key}>
+    <App />
+  </LoadScript>
 );
+//   <React.StrictMode>
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
