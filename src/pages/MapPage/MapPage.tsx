7a988bf17c1e8b982c4a784b6734a5ea71280888
Lingess Rajoo <Lrajoo@purdue.edu>
2022-11-04 23:51:53 -0700

    Added markers and Add Location Button

--- src/pages/MapPage/MapPage.tsx
+++ src/pages/MapPage/MapPage.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
-import { Col, Row, Layout, Button } from "antd";
-import { MenuOutlined, CloseOutlined } from "@ant-design/icons";
+import { Layout, Button } from "antd";
 import { GoogleMap, LoadScript, Marker } from "@react-google-maps/api";
+import { useNavigate } from "react-router-dom";
 import Header from "../../components/Header/Header";
 import Sidebar from "../../components/Sider/Sidebar";
 import "./MapPage.css";
@@ -11,20 +11,80 @@ const { Content } = Layout;
 const MapPage = () => {
   const [sidebarCollapsed, setSidebarCollapsed] = useState(true);
 
+  let navigate = useNavigate();
+
   const toggleSidebarView = (collapsed: boolean) => {
     setSidebarCollapsed(collapsed);
   };
 
   const center = {
-    lat: 40.754932,
-    lng: -73.984016,
+    lat: 40.7309,
+    lng: -73.9973,
   };
 
-  // const position = {
-  //   lat: 40.7128,
-  //   lng: 74.006,
-  // };
+  const currentLocation = {
+    lat: 40.723,
+    lng: -73.9934,
+  };
 
+  const places = [
+    {
+      key: "1",
+      position: {
+        lat: 40.739111,
+        lng: -73.989113,
+      },
+      icon: "https://img.icons8.com/fluency/35/null/coffee-to-go.png",
+    },
+    {
+      key: "2",
+      position: {
+        lat: 40.7160576,
+        lng: -73.9646899,
+      },
+      icon: "https://img.icons8.com/fluency/35/null/coffee-to-go.png",
+    },
+    {
+      key: "3",
+      position: {
+        lat: 40.6885567,
+        lng: -73.98345909999999,
+      },
+      icon: "https://img.icons8.com/fluency/35/null/coffee-to-go.png",
+    },
+    {
+      key: "4",
+      position: {
+        lat: 40.7138484,
+        lng: -73.9909298,
+      },
+      icon: "https://img.icons8.com/external-bearicons-blue-bearicons/35/null/external-Restaurant-location-bearicons-blue-bearicons.png",
+    },
+    {
+      key: "5",
+      position: {
+        lat: 40.7049369,
+        lng: -74.00973429999999,
+      },
+      icon: "https://img.icons8.com/external-bearicons-blue-bearicons/35/null/external-Restaurant-location-bearicons-blue-bearicons.png",
+    },
+    {
+      key: "6",
+      position: {
+        lat: 40.7032685,
+        lng: -74.01102179999999,
+      },
+      icon: "https://img.icons8.com/external-vitaliy-gorbachev-flat-vitaly-gorbachev/35/null/external-cocktail-vacation-vitaliy-gorbachev-flat-vitaly-gorbachev-1.png",
+    },
+    {
+      key: "7",
+      position: {
+        lat: 40.7421711,
+        lng: -74.00352199999999,
+      },
+      icon: "https://img.icons8.com/external-vitaliy-gorbachev-flat-vitaly-gorbachev/35/null/external-cocktail-vacation-vitaliy-gorbachev-flat-vitaly-gorbachev-1.png",
+    },
+  ];
   return (
     <Layout>
       <Sidebar collapsed={sidebarCollapsed} toggleSidebarView={toggleSidebarView} />
@@ -42,9 +102,22 @@ const MapPage = () => {
               zoomControlOptions: { position: 6.0 },
             }}
           >
-            {/* <Marker position={position} /> */}
+            <Marker
+              position={currentLocation}
+              icon="https://img.icons8.com/external-anggara-blue-anggara-putra/35/null/external-user-user-interface-basic-anggara-blue-anggara-putra.png"
+            />
+            {places.map((place) => {
+              return <Marker key={place.key} icon={place.icon} position={place.position} />;
+            })}
           </GoogleMap>
         </LoadScript>
+        <Button
+          type="primary"
+          style={{ position: "absolute", bottom: "4vh", right: "2vh" }}
+          onClick={() => navigate("/search")}
+        >
+          Add Location
+        </Button>
       </Content>
     </Layout>
   );
