632fc4a514f99758348bb8ab275a750533e95f8f
Lingess Rajoo <Lrajoo@purdue.edu>
2022-11-07 22:48:01 -0800

    Removed extra / in api call

--- src/pages/SearchPage/SearchPageService.tsx
+++ src/pages/SearchPage/SearchPageService.tsx
@@ -3,11 +3,11 @@ import axios from "axios";
 const api = process.env.REACT_APP_BACKEND_API_URL || "http://localhost:3001";
 
 export const getSearchResults = async (searchQuery: string) => {
-  const res = await axios.get(`${api}/mappy/api/places?search=${searchQuery}`);
+  const res = await axios.get(`${api}mappy/api/places?search=${searchQuery}`);
   return res;
 };
 
 export const getPlaceDetails = async (placeID: string) => {
-  const res = await axios.get(`${api}/mappy/api/place/${placeID}`);
+  const res = await axios.get(`${api}mappy/api/place/${placeID}`);
   return res;
 };
